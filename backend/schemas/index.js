4e5792e23439f6a55bf8468abcb7d879d67b6758
Bruno Matheus <bmatheusc@gmail.com>
2022-08-22 23:38:55 -0300

    Initial commit and backend config

---
+++ backend/schemas/index.js
@@ -0,0 +1,5 @@
+export { default as comment } from './comment';
+export { default as pin } from './pin';
+export { default as postedBy } from './postedBy';
+export { default as save } from './save';
+export { default as user } from './user';
